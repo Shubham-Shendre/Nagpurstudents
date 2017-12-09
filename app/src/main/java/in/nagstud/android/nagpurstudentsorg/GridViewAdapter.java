67674089ce7534350bd9ab72c23b1031cb6816cf
SdS(aka Shubham Dilip Shendre) <shubhamshendreofficial@gmail.com>
2017-12-09 19:18:06 +0530

    once again dec 9 7.25pm

---
+++ app/src/main/java/in/nagstud/android/nagpurstudentsorg/GridViewAdapter.java
@@ -0,0 +1,53 @@
+package in.nagstud.android.nagpurstudentsorg;
+//not using this class adapter
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+/**
+ * Created by shubham dilip shendre aka SdS for nagstud Project nagpurstudents
+ */
+public class GridViewAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public GridViewAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {  // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+//pass the data to the gridviewadapter to shw on the page
+    private Integer[] mThumbIds = {
+            R.drawable.adele, R.drawable.adele,
+            R.drawable.adele, R.drawable.adele,
+            R.drawable.adele, R.drawable.adele,
+            R.drawable.adele, R.drawable.adele,
+
+    };
+}
